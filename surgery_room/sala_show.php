7de6e80351caa2c890291f91c88fa96a93c45daa
Pedro Sudario <pedrohenriquesudariosilva@gmail.com>
2023-11-18 20:24:49 -0300

    Stored procedure

--- surgery_room/sala_show.php
+++ surgery_room/sala_show.php
@@ -8,6 +8,7 @@ include("../components/nav.php");
     <?php
     $id = $_GET["ID_Sala"];
     $sql = "SELECT ID_Sala, Bloco_Sala, Numero_Sala, tipo.tipo FROM Sala INNER JOIN Tipo ON fk_Tipo_ID_tipo = ID_tipo WHERE ID_Sala = $id";
+    $procedure = "CALL SalaDeCirurgia($id)";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
@@ -16,6 +17,17 @@ include("../components/nav.php");
             echo "<p class='text-lg mb-2'><strong>Bloco:</strong> {$row['Bloco_Sala']}</p>";
             echo "<p class='text-lg mb-2'><strong>Numero:</strong> {$row['Numero_Sala']}</p>";
             echo "<p class='text-lg mb-2'><strong>Tipo:</strong> {$row['tipo']}</p>";
+        }
+    }
+
+    $result_procedure = $conn->query($procedure);
+
+    if ($result_procedure->num_rows > 0) {
+        while ($row = $result_procedure->fetch_assoc()) {
+            echo "<h2 class='text-2xl font-bold mb-4'>Cirurgia(s) em andamento</h2>";
+            echo "<p class='text-lg mb-2'><strong>Nome do médico:</strong> {$row['Nome_Medico']}</p>";
+            echo "<p class='text-lg mb-2'><strong>Nome do paciente:</strong> {$row['Nome_Paciente']}</p>";
+            echo "<p class='text-lg mb-2'><strong>Tipo da cirurgia:</strong> {$row['tipo']}</p>";
         }
     }
     ?>
