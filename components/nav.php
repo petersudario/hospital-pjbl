0b3794fda5b1a134fb5c2ab35dc073c16797083d
SkrdKm <akemiskrd@gmail.com>
2023-11-18 01:46:11 -0300

    navagation

--- components/nav.php
+++ components/nav.php
@@ -5,18 +5,18 @@
         <span class="font-semibold text-xl tracking-tight">Sakurada Hospital</span>
     </div>
     <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div class="text-sm lg:flex-grow">
-            <form action="toHome.php" method="POST">
+        <div class="flex text-sm lg:flex-grow">
+            <form action="/hospital-pjbl/components/toHome.php" method="POST">
                 <button type="submit" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                     Home
                 </button>
             </form>
-            <form action="/hospital-pjbl/hospital-pjbl/components/toMedic.php" method="POST">
+            <form action="/hospital-pjbl/components/toMedic.php" method="POST">
                 <button type="submit" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                     Medics
                 </button>
             </form>
-            <form action="/hospital-pjbl/hospital-pjbl/components/toNurse.php" method="POST">
+            <form action="/hospital-pjbl/components/toNurse.php" method="POST">
                 <button type="submit" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                     Nurses
                 </button>
